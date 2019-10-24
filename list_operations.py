a2ce28014111faab13aa6b95d7b4f37f52960639
apugithub <sgupdate1988@gmail.com>
2019-10-24 16:20:14 +0530

    24thOct'19

--- list_operations.py
+++ list_operations.py
@@ -6,11 +6,16 @@ print(planets)
 
 print(planets[-1:-2])   # This will not give any o/p as the traverse can't be done reverse, its always from left to right
 print (planets[-2:-1])  # So this one will return result
-
 print(sorted(planets))
+print(planets[-5:-2])
+numbs = [3,4,5]
+a ='san'
+print(sum(numbs))
+print(planets.index('Mars'))
+print(a in planets)  # Nice feature....boolean.
 
-a = 10 + 3.34j
-print (a.imag)
+
+
 
 
 
--- send_email(HTML).py
+++ send_email(HTML).py
@@ -6,7 +6,7 @@ from email.mime.text import MIMEText
 
 from_addr = '9176833080apu@gmail.com'
 to_addr = ["9176833080apu@gmail.com", "ghosh.santanu30@gmail.com"]  #the recipients in sendmail(sender, recipients, message) needs to be a list:
-text = "Hello this is test mail from Python"
+text = "Hello this is test mail from Python in PLAIN format"
 
 username = from_addr
 password = '9176833080'
@@ -15,8 +15,24 @@ msg = MIMEMultipart()   # Through multipart tou can separate
 
 msg['From'] = from_addr
 msg['To'] = "9176833080apu@gmail.com, ghosh.santanu30@gmail.com"   #msg['To'] needs to be a string
-msg['Subject'] = 'Test Mail_Test'
-msg.attach(MIMEText(text))
+msg['Subject'] = 'Test Mail_HTML'
+
+html = """\
+<html>
+  <head></head>
+  <body>
+    <p>Hi!<br>
+       How are you?<br>
+       Here is the <a href="http://www.python.org">link</a> you wanted.
+    </p>
+  </body>
+</html>
+"""
+
+#'Attach' can be done multiple times. And the format is   msg.attach(MIMEText(<variable_name>, '<format>'))
+msg.attach(MIMEText(html, 'html'))
+msg.attach(MIMEText(text, 'plain'))
+
 
 server = smtplib.SMTP('smtp.gmail.com:587')   #This can be written as server = SMTP (...)   if you import SMTP already
 server.ehlo()
