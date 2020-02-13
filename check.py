d0180de9c70a49f23fecb1adcac081cb206a2810
apugithub <sgupdate1988@gmail.com>
2020-02-13 10:15:34 +0530

    13th Feb

--- check.py
+++ check.py
@@ -1,20 +1,5 @@
-a = [1,2,3,4]
-def elementwise_greater_than(L, thresh):
-    """Return a list with the same length as L, where the value at index i is
-    True if L[i] is greater than thresh, and False otherwise.
+import pandas as pd
 
-    #>>> elementwise_greater_than([1, 2, 3, 4], 2)
-    [False, False, True, True]
-    """
-    return  [num%thresh ==0 for num in L]
+df = pd.read_csv('data/pokemon_data.csv')
 
-    pass
-
-
-print(elementwise_greater_than(a,2))
-
-
-
-def has_lucky_number(nums):
-    return any([num % 7 == 0 for num in nums])
-
+print(df.info())
---
+++ pandas_intro.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv('data/pokemon_data.csv') #Dataframe (the core of pandas)
+
+#print(df)  # Print the dataframe
+#print(df.info()) #Prints the summary of dataframe
+
+#print(df.head()) # head prints first 5 records  + you may specify number inside head too
+
+#print(df['HP'].max())  # displaying the max value of the column HP
+
+#print(df.columns) # prints the column names
+
+#print (df[['HP','Name']]) #Multiple columns... by specifying a list
+
+#print(df['HP'][10:15])
+
+print(df.iloc[0:4,[0,1,2,3]])  # 0:4 is the index range of rows and [0,1,2,3] are the position of columns like 1st, 2nd
+
+
+
+
