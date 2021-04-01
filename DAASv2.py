0239f5f9dd021ba27dc7f8ae9c6c1f268852a8bd
apugithub <apugithub@users.noreply.github.com>
2021-04-01 08:10:42 +0530

    Update DAASv2.py

--- DAASv2.py
+++ DAASv2.py
@@ -1,5 +1,23 @@
 ## There code will merge all the table and add all the links and filters
 
+'''from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql.functions import *   
+
+
+#Initializing spark session
+spark = SparkSession.builder.appName("DAAS") \
+.config("hive.exec.dynamic.partition", "true") \
+.config("hive.exec.dynamic.partition.mode", "nonstrict") \
+.config("spark.sql.warehouse.dir", "/apps/hive/warehouse") \
+.config("spark.sql.catalogImplementation","in-memory") \
+.config("spark.sql.shuffle.partitions","20") \
+.enableHiveSupport() \
+.getOrCreate()
+
+#setloglevel 
+spark.sparkContext.setLogLevel("ERROR")
+'''
 
 
 def fw (**kargs):
@@ -15,10 +33,11 @@ def fw (**kargs):
             
         df = "select * from " + ', '.join(i for i in kargs['tables']) + " on " + ' and '.join(i for i in kargs['linkage']) + " where " + ' and '.join(i for i in kargs['filter'])
         print (df)
-        #spark.sql(df)
+        #df1 = spark.sql(df)
 
 dct = { 'tables' : ['t1','t2','t3'], 'linkage' : ['link1', 'link2', 'link3'], 'filter' : ['1=1','filter1', 'filter2'] }
 
 # Accessing dictionary elements and passing to the function
 fw(tables = dct['tables'], linkage= dct['linkage'], filter= dct['filter'])
+
 
