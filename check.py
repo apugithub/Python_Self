62211be53247ee8ec3ace812e8e135980f30a9d3
apugithub <sgupdate1988@gmail.com>
2021-06-26 14:40:14 +0530

    Todoist & google task added

--- check.py
+++ check.py
@@ -1,17 +1,1 @@
-import requests
-import json
-
-url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
-
-querystring = {"q":"Oslo","days":"5"}
-
-headers = {
-    'x-rapidapi-key': "caca550ce0msh841b31180f7b290p13c608jsn6b9a68723b06",
-    'x-rapidapi-host': "weatherapi-com.p.rapidapi.com"
-    }
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-h = requests.get(url=url, params=querystring, headers=headers)
-data = response.json()
-print(response.json())
 
